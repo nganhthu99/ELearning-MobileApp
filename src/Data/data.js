d05821f3531d23a10b8146b63e443d3715759646
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-10 22:31:41 +0700

    Add sample data

---
+++ src/Data/data.js
@@ -0,0 +1,264 @@
+export const courses = [
+    {
+        id: 1,
+        title: 'React Native',
+        author: 'Hai Pham',
+        level: 'Advanced',
+        release: 'Sep 25, 2020',
+        duration: '38 hours'
+    },
+    {
+        id: 2,
+        title: 'React',
+        author: 'Huy Nguyen',
+        level: 'Advanced',
+        release: 'Oct 16, 2020',
+        duration: '50 hours'
+    },
+    {
+        id: 3,
+        title: 'Algorithms',
+        author: 'Phuong Nguyen',
+        level: 'Intermediate',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 4,
+        title: 'Statistics',
+        author: 'Hoang Vu',
+        level: 'Intermediate',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 5,
+        title: 'Java Programming',
+        author: 'Thanh Ho',
+        level: 'Advanced',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 6,
+        title: 'Software Process',
+        author: 'Huy Nguyen',
+        level: 'Advanced',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 7,
+        title: 'Android Programming',
+        author: 'Thinh Truong',
+        level: 'Advanced',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 8,
+        title: 'IOS Programming',
+        author: 'Hai Pham',
+        level: 'Advanced',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 9,
+        title: 'C/C++',
+        author: 'Huy Nguyen',
+        level: 'Advanced',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    },
+    {
+        id: 10,
+        title: 'Object-Oriented Programming',
+        author: 'Huy Nguyen',
+        level: 'Advanced',
+        release: 'Oct 1, 2020',
+        duration: '33 hours'
+    }
+]
+
+export const authors = [
+    {
+        id: 1,
+        name: 'Huy Nguyen',
+        courses: 4
+    },
+    {
+        id: 2,
+        name: 'Hai Pham',
+        courses: 5
+    },
+    {
+        id: 3,
+        name: 'Khanh Nguyen',
+        courses: 6
+    },
+    {
+        id: 4,
+        name: 'Ho Thanh',
+        courses: 6
+    },
+    {
+        id: 5,
+        name: 'Phuong Nguyen',
+        courses: 6
+    },
+    {
+        id: 6,
+        name: 'Hoang Vu',
+        courses: 6
+    }
+]
+
+export const topics = [
+    {
+        id: 1,
+        title: 'REACT NATIVE',
+    },
+    {
+        id: 2,
+        title: 'REACT',
+    },
+    {
+        id: 3,
+        title: 'JAVA',
+    },
+    {
+        id: 4,
+        title: 'C/C++',
+    },
+    {
+        id: 5,
+        title: 'ALGORITHM',
+    },
+    {
+        id: 6,
+        title: 'DATA STRUCTURE',
+    },
+    {
+        id: 7,
+        title: 'LOGICAL THINKING',
+    },
+    {
+        id: 8,
+        title: 'OPERATING SYSTEM',
+    },
+    {
+        id: 9,
+        title: 'STATISTICS',
+    },
+    {
+        id: 10,
+        title: 'NETWORK',
+    },
+]
+
+export const searchHistory=[
+    {
+        id: 1,
+        content: 'react native',
+    },
+    {
+        id: 2,
+        content: 'react',
+    },
+    {
+        id: 3,
+        content: 'java',
+    },
+    {
+        id: 4,
+        content: 'javascript',
+    },
+    {
+        id: 5,
+        content: 'c++',
+    },
+    {
+        id: 6,
+        content: 'data structure',
+    },
+    {
+        id: 7,
+        content: 'stack',
+    },
+    {
+        id: 8,
+        content: 'dynamic programming',
+    },
+    {
+        id: 9,
+        content: 'algorithm',
+    },
+    {
+        id: 5,
+        content: 'python',
+    }
+]
+
+export const comments = [
+    {
+        id: 1,
+        time: 'Nov 01, 2020',
+        user: 'nganhthu',
+        content: 'This course is so informative'
+    },
+    {
+        id: 2,
+        time: 'Nov 02, 2020',
+        user: 'ddtuan',
+        content: 'This course is so informative'
+    },
+    {
+        id: 3,
+        time: 'Nov 03, 2020',
+        user: 'nhtnhi',
+        content: 'This course is so informative'
+    },
+    {
+        id: 4,
+        time: 'Nov 04, 2020',
+        user: 'hthien',
+        content: 'This course is so informative'
+    },
+    {
+        id: 5,
+        time: 'Nov 05, 2020',
+        user: 'nmdung',
+        content: 'This course is so informative'
+    },
+    {
+        id: 6,
+        time: 'Nov 06, 2020',
+        user: 'ncthanh',
+        content: 'This course is so informative'
+    },
+    {
+        id: 7,
+        time: 'Nov 07, 2020',
+        user: 'ltnhu',
+        content: 'This course is so informative'
+    },
+    {
+        id: 8,
+        time: 'Nov 08, 2020',
+        user: 'ntcong',
+        content: 'This course is so informative'
+    },
+    {
+        id: 9,
+        time: 'Nov 09, 2020',
+        user: 'nnkqui',
+        content: 'This course is so informative'
+    },
+    {
+        id: 10,
+        time: 'Nov 10, 2020',
+        user: 'dkngoc',
+        content: 'This course is so informative'
+    }
+]
