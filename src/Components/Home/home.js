727e2d6a0497b1eba87a4da5c813816be8e34e19
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-11 22:05:23 +0700

    Refactor home screen

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,69 +1,120 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import SectionList from "./SectionView/section-list";
-import List from "./ListView/list";
-import ListItem from "./ListView/list-item";
+import {ScrollView, StyleSheet, View} from "react-native";
+import {authors, courses, topics} from "../../Data/data";
+import ImageButton from "../Common/image-button";
+import VerticalCourseList from "../CoursesList/VerticalCourseList/vertical-course-list";
+import HorizontalTopicList from "./horizontal-topic-list";
+import SectionFooter from "../Common/section-footer";
+import SectionHeader2 from "../Common/section-header-2";
+import HorizontalAuthorList from "../AuthorList/HorizontalAuthorList/horizontal-author-list";
+import SectionHeader from "../Common/section-header";
+
 
 const Home = (props) => {
-    const courses = [
-        {
-            id: 1,
-            title: 'React Native',
-            author: 'Hai Pham',
-            level: 'Advanced',
-            release: 'Sep 25, 2020',
-            duration: '38 hours'
-        },
-        {
-            id: 2,
-            title: 'React',
-            author: 'Huy Nguyen',
-            level: 'Advanced',
-            release: 'Oct 16, 2020',
-            duration: '50 hours'
-        },
-        {
-            id: 3,
-            title: 'Design Pattern',
-            author: 'Huy Nguyen',
-            level: 'Advanced',
-            release: 'Oct 1, 2020',
-            duration: '33 hours'
-        }
-    ]
+    const handleRecommendButton = () => {
+        props.navigation.navigate('CourseList', {
+            header: 'Recommended Courses',
+            items: courses
+        })
+    }
 
-    const channels = [
-        {
-            id: 1,
-            name: 'React Native',
-            amount: 3,
-        },
-        {
-            id: 2,
-            name: 'React',
-            amount: 2,
-        },
-        {
-            id: 3,
-            name: 'Java Programming',
-            amount: 6,
-        }
-    ]
+    const handleNewButton = () => {
+        props.navigation.navigate('CourseList', {
+            header: 'New Courses',
+            items: courses
+        })
+    }
 
-    return (
+    const handleSeeAllTopCoursesButton = () => {
+        props.navigation.navigate('CourseList', {
+            header: 'Top Courses',
+            items: courses
+        })
+    }
+
+    const handleAllAuthorsButton = () => {
+        props.navigation.navigate('AuthorList', {
+            header: 'Authors',
+            items: authors
+        })
+    }
+
+    return(
         <ScrollView style={styles.container}>
-            <SectionList items={courses} title='Continue Learning' type='courses'/>
-            <SectionList items={courses} title='My Favorites' type='courses'/>
-            <SectionList items={channels} title='My Channel' type='channels'/>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <ImageButton handleOnClick={handleRecommendButton}
+                                 title='RECOMMEND FOR YOU'
+                                 image={require('../../../assets/background_1.jpg')}/>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <ImageButton handleOnClick={handleNewButton}
+                                 title='NEW RELEASE'
+                                 image={require('../../../assets/background_2.jpg')}/>
+                </View>
+            </View>
+            <View style={styles.hotTopicsContainer}>
+                <View style={styles.header}>
+                    <SectionHeader2 title='Hot Topics'/>
+                </View>
+                <View style={{paddingTop: 10}}>
+                    <HorizontalTopicList navigation={props.navigation} items={topics}/>
+                </View>
+            </View>
+
+            <View style={styles.topCoursesContainer}>
+                <View style={styles.header}>
+                    <SectionHeader2 title='Top Courses'/>
+                </View>
+                <View>
+                    <VerticalCourseList navigation={props.navigation} items={courses}/>
+                </View>
+                <View>
+                    <SectionFooter handleOnClick={handleSeeAllTopCoursesButton}/>
+                </View>
+            </View>
+
+            <View style={styles.topAuthorsContainer}>
+                <View style={styles.header}>
+                    <SectionHeader title='Top Authors' handleOnClick={handleAllAuthorsButton}/>
+                </View>
+                <View>
+                    <HorizontalAuthorList
+                        navigation={props.navigation}
+                        items={authors}/>
+                </View>
+            </View>
         </ScrollView>
-    );
-}
+    )
+};
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#F2F2F2',
     },
+    buttonsContainer: {
+        height: 240,
+        padding: 5
+    },
+    buttonContainer: {
+        flex: 1,
+        paddingBottom: 5,
+    },
+    hotTopicsContainer: {
+        padding: 5,
+        paddingTop: 15,
+    },
+    topCoursesContainer: {
+        padding: 5,
+        paddingTop: 15,
+    },
+    topAuthorsContainer: {
+        padding: 5
+    },
+    header: {
+        paddingTop: 10,
+        paddingBottom: 10
+    }
 });
 
 export default Home;
