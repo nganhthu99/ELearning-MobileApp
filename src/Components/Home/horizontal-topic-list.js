f7beeda086c966404861a6c7e72d321cb4be1b44
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-11 08:22:25 +0700

    Refactor topics list

---
+++ src/Components/Home/horizontal-topic-list.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {ScrollView, StyleSheet, View} from "react-native";
+import ImageButton from "./image-button";
+import {courses} from "../../Data/data";
+
+const HorizontalTopicList = (props) => {
+    return (
+        <View style={styles.container}>
+            <ScrollView horizontal={true}>
+                {props.items.map((item) => {
+                    return (
+                        <View key={item.id} style={{height: 150, width: 200, paddingRight: 5}}>
+                            <ImageButton
+                                handleOnClick={() => {
+                                    props.navigation.navigate('CourseList', {
+                                    header: item.title,
+                                    items: courses
+                                })}}
+                                key={item.id}
+                                title={item.title}
+                                image={require('../../../assets/course.jpg')}/>
+                        </View>
+                    )
+                })}
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+});
+
+
+export default HorizontalTopicList;
