1d3df6ed9d793cdbbbffe957e9f5d081e9b6a18c
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-22 19:56:25 +0700

    Add file constants (saved screens’ name)

---
+++ src/Globals/constants.js
@@ -0,0 +1,31 @@
+export const ScreenName = {
+    SplashScreen: "SplashScreen",
+    StartMenu: "StartMenu",
+    SignUp: "SignUp",
+    SignIn: "SignIn",
+    ForgetPassword: "ForgetPassword",
+    MainTab: "MainTab",
+    HomeStack: "HomeStack",
+    MyCoursesStack: "MyCoursesStack",
+    SearchStack: "SearchStack",
+    AccountStack: "AccountStack",
+    Home: "Home",
+    CourseList: "CourseList",
+    CourseDetail: "CourseDetail",
+    AuthorList: "AuthorList",
+    AuthorDetail: "AuthorDetail",
+    MyCourses: "MyCourses",
+    Search: "Search",
+    SearchResult: "SearchResult",
+    SearchResultAll: "SearchResultAll",
+    Account: "Account",
+    CourseDetailInformation: "CourseDetailInformation",
+    CourseDetailLesson: "CourseDetailLesson",
+    CourseDetailExercise: "CourseDetailExercise",
+    CourseDetailComment: "CourseDetailComment"
+}
+
+export const ImageName = {
+    LogoImage: "",
+    LogoText: "",
+}
