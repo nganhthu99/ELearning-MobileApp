07ab808736285c6182be31152c840dafc176b2a2
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-24 08:21:23 +0700

    Add file constant defined languages and themes

---
+++ src/Globals/languages.js
@@ -0,0 +1,144 @@
+export const languages = {
+    english: {
+        forgetPwInstruction: 'Enter your registered email to reset password',
+        forgetPwEmailInput: 'Registered Email',
+        forgetPwEmailButton: 'Send Email Verification',
+
+        usernameValidate: 'Username should have at least 6 characters',
+        emailValidate: 'Invalid email address',
+        passwordValidate: 'Password should have at least 8 characters',
+        confirmPasswordValidate: 'Confirm password should be matched',
+
+        usernameInput: 'Username',
+        passwordInput: 'Password',
+        forgetPwButton: 'Forget password?',
+        signIn: 'Sign In',
+        or: 'OR',
+        googleSignIn: 'Sign In With Google',
+
+        emailInput: 'Email',
+        confirmPasswordInput: 'Confirm Password',
+        signUp: 'Sign Up',
+
+        explore: 'Explore',
+
+        home: 'Home',
+        recommendForYou: 'RECOMMEND FOR YOU',
+        newRelease: 'NEW RELEASE',
+        hotTopics: 'Hot Topics',
+        topCourses: 'Top Courses',
+        seeAll: 'See All',
+        topAuthors: 'Top Authors',
+
+        myCourses: 'My Courses',
+        continueLearning: 'Continue Learning',
+        registered: 'Registered',
+        downloaded: 'Downloaded',
+        favourite: 'Favourite',
+
+        search: 'Search',
+        recentSearches: 'Recent Searches',
+        deleteAll: 'Delete All',
+        all: 'All',
+        authors: 'Authors',
+        courses: 'Courses',
+
+        account: 'Account',
+        changePassword: 'Change Password',
+        settings: 'Settings',
+        blueMode: 'Blue Mode',
+        language: 'Language',
+        vietnamese: 'Vietnamese',
+        english: 'English',
+        signOut: 'Sign Out',
+
+        courseDetail: 'Course Detail',
+        information: 'INFO...',
+        lesson: 'LESSON',
+        exercise: 'EXER...',
+        comment: 'COM...',
+        rating: 'Rating',
+        relatedCourses: 'Related Courses',
+
+        listCourses: 'List Courses',
+        listAuthors: 'List Authors',
+        authorDetail: 'Author Detail',
+
+        allTab: 'All',
+
+        searchResult: 'Search Result',
+
+        commentInput: 'Comment',
+    },
+    vietnamese: {
+        forgetPwInstruction: 'Nhập Email đăng ký để khôi phục mật khẩu',
+        forgetPwEmailInput: 'Email',
+        forgetPwEmailButton: 'Gửi Email xác nhận',
+
+        usernameValidate: 'Tên đăng nhập cần có ít nhất 6 kí tự',
+        emailValidate: 'Email không hợp lệ',
+        passwordValidate: 'Mật khẩu cần có ít nhật 8 kí tự',
+        confirmPasswordValidate: 'Mật khẩu xác minh không trùng',
+
+        usernameInput: 'Tên đăng nhập',
+        passwordInput: 'Mật khẩu',
+        forgetPwButton: 'Quên mật khẩu?',
+        signIn: 'Đăng Nhập',
+        or: 'Hoặc',
+        googleSignIn: 'Đăng Nhập Bằng Google',
+
+        emailInput: 'Email',
+        confirmPasswordInput: 'Xác minh email',
+        signUp: 'Đăng Ký',
+
+        explore: 'Khám Phá',
+
+        home: 'Trang Chủ',
+        recommendForYou: 'GỢI Ý CHO BẠN',
+        newRelease: 'KHÓA HỌC MỚI',
+        hotTopics: 'Chủ Đề Xu Hướng',
+        topCourses: 'Khoá Học Nổi Bật',
+        seeAll: 'Xem Tất Cả',
+        topAuthors: 'Tác Giả Nổi Bật',
+
+        myCourses: 'Khoá Học Của Tôi',
+        continueLearning: 'Tiếp Tục Học',
+        registered: 'Khoá Học Đã Đăng Ký',
+        downloaded: 'Khóa Học Đã Tải',
+        favourite: 'Khoá Học Yêu Thích',
+
+        search: 'Tìm kiếm',
+        recentSearches: 'Tìm kiếm gần đây',
+        deleteAll: 'Xoá Tất Cả',
+        all: 'TẤT CẢ',
+        authors: 'TÁC GIẢ',
+        courses: 'KHÓA HỌC',
+
+        account: 'Tài Khoản',
+        changePassword: 'Thay Đổi Mật Khẩu',
+        settings: 'Cài Đặt',
+        blueMode: 'Chế độ Xanh',
+        language: 'Ngôn Ngữ',
+        vietnamese: 'Tiếng Việt',
+        english: 'Tiếng Anh',
+        signOut: 'Đăng Xuất',
+
+        courseDetail: 'Chi Tiết Khoá Học',
+        information: 'TỔNG QUAN',
+        lesson: 'BÀI HỌC',
+        exercise: 'BÀI TẬP',
+        comment: 'BÌNH LUẬN',
+        rating: 'ĐÁNH GIÁ',
+        relatedCourses: 'Khoá Học Tương Tự',
+
+        listCourses: 'Danh Sách Khoá Học',
+        listAuthors: 'Danh Sách Tác Giả',
+        authorDetail: 'Chi Tiết Tác Giả',
+
+        allTab: 'Tất Cả',
+
+        searchResult: 'Kết Quả Tìm Kiếm',
+
+        commentInput: 'Bình luận'
+    }
+}
---
+++ src/Globals/themes.js
@@ -0,0 +1,26 @@
+export const themes = {
+    light: {
+        background: '#F2F2F2',
+        navigationBar: '#AED3F2',
+        listDivider: '#AED3F2',
+        primaryButton: '#2089DC',
+        star: '#0E66EE',
+        header: '#155FBF',
+        primaryEmphasis: '#021F59',
+        normalText: '#011534',
+        secondaryButton: '#A62103',
+        secondaryEmphasis: '#590202',
+    },
+    dark: {
+        background: '#000000',
+        navigationBar: '#000000',
+        listDivider: '#AED3F2',
+        primaryButton: '#7FC9FF',
+        star: '#17D1F8',
+        header: '#71B5F4',
+        primaryEmphasis: '#AED3F2',
+        normalText: '#F2F2F2',
+        secondaryButton: '#F8A290',
+        secondaryEmphasis: '#FB795E',
+    }
+}
