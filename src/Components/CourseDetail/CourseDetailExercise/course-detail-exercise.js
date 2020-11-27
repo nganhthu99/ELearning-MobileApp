43e0b5d70f596abf467b29d8636906b038977520
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-27 10:50:57 +0700

    Adding course detail exercises and lessons

--- src/Components/CourseDetail/CourseDetailExercise/course-detail-exercise.js
+++ src/Components/CourseDetail/CourseDetailExercise/course-detail-exercise.js
@@ -1,22 +1,50 @@
-import React, {useContext} from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import React, {useContext, useState} from 'react';
+import {ScrollView, StyleSheet, Text, View} from "react-native";
 import {ThemeContext} from "../../../Provider/theme-provider";
+import Accordion from 'react-native-collapsible/Accordion';
+import {courses} from "../../../Data/data";
 
-const CourseDetailExercise = (props) => {
+const CourseDetailExercise= (props) => {
     const {theme} = useContext(ThemeContext)
+    const [activeSections, setActiveSections] = useState([])
+
+    const SECTIONS = courses[0].exercise
+
+    const _renderHeader = section => {
+        return (
+            <View style={{borderTopWidth: 1, borderTopColor: theme.primaryButton, borderBottomWidth: 1, borderBottomColor: theme.primaryButton, backgroundColor: theme.primaryButton, justifyContent:'center', paddingTop: 10, paddingBottom: 10}}>
+                <Text style={{color: theme.background, fontSize: 16, fontWeight: 'bold', paddingLeft: 5, paddingRight: 5}}>{`${section.id}.  ${section.title}`}</Text>
+            </View>
+        );
+    };
+
+    const _renderContent = section => {
+        return (
+            <View style={{paddingLeft: 5, paddingRight: 5, paddingTop: 10}}>
+                <Text style={{color: theme.normalText}}>{section.content}</Text>
+            </View>
+        );
+    };
+
+    const _updateSections = activeSections => {
+        setActiveSections(activeSections);
+    };
 
     return (
-        <View style={styles(theme).container}>
-            <Text style={{color: theme.normalText}}>
-                Course Detail Exercise
-            </Text>
-        </View>
+        <ScrollView style={styles(theme).container}>
+            <Accordion
+                sections={SECTIONS}
+                activeSections={activeSections}
+                renderHeader={_renderHeader}
+                renderContent={_renderContent}
+                onChange={_updateSections}
+            />
+        </ScrollView>
     )
 };
 
 const styles = (theme) => StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: theme.background,
     },
 });
