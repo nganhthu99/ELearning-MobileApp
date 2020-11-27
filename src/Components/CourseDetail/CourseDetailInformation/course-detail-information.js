a3fd8325dc4be3c6e8846ecfe3a6bb7b85e7da70
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-27 10:52:15 +0700

    Update course detail information, adding see-more text component

--- src/Components/CourseDetail/CourseDetailInformation/course-detail-information.js
+++ src/Components/CourseDetail/CourseDetailInformation/course-detail-information.js
@@ -1,59 +1,177 @@
-import React, {useContext} from 'react';
-import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import StarRating from "react-native-star-rating";
+import React, {useContext, useEffect, useState} from 'react';
+import {Alert, Button, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {Icon} from "react-native-elements";
 import HorizontalCourseList from "../../CoursesList/HorizontalCourseList/horizontal-course-list";
 import {courses} from "../../../Data/data";
 import CourseInfo from "../../CoursesList/course-info";
 import SectionHeader2 from "../../Common/section-header-2";
 import {ThemeContext} from "../../../Provider/theme-provider";
-import {LanguageContext} from "../../../Provider/language-provider";
+import {RegisteredCoursesContext} from "../../../Provider/registered-courses-provider";
+import {DownloadedCoursesContext} from "../../../Provider/downloaded-courses-provider";
+import {FavouriteCoursesContext} from "../../../Provider/favourite-courses-provider";
+import {AuthenticationContext} from "../../../Provider/authentication-provider";
+import ViewMoreText from "react-native-view-more-text";
 
 const CourseDetailInformation = (props) => {
+    const item = props.route.params.item
     const {theme} = useContext(ThemeContext)
-    const {language} = useContext(LanguageContext)
+    const {authentication} = useContext(AuthenticationContext)
+    const {registeredCourses, setRegisteredCourses} = useContext(RegisteredCoursesContext)
+    const {downloadedCourses, setDownloadedCourses} = useContext(DownloadedCoursesContext)
+    const {favouriteCourses, setFavouriteCourses} = useContext(FavouriteCoursesContext)
+
+    const [isRegistered, setIsRegistered] = useState(
+        (registeredCourses.some(returnItem => returnItem.id === item.id))
+    )
+
+    const [isDownloaded, setIsDownloaded] = useState(
+        (downloadedCourses.some(returnItem => returnItem.id === item.id))
+    )
+    const [isFavourite, setIsFavourite] = useState(
+        (favouriteCourses.some(returnItem => returnItem.id === item.id))
+    )
+
+    const alert = () => Alert.alert(
+        'Authentication Error',
+        'Please Sign In or Sign Up to use this feature',
+        [
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel'
+            }
+        ]
+    );
+
+    const handleRegisteredButton = () => {
+        if (authentication) setIsRegistered(!isRegistered)
+        else {
+            alert()
+        }
+    }
+
+    const handleDownloadedButton = () => {
+        if (authentication) setIsDownloaded(!isDownloaded)
+        else {
+            alert()
+        }
+    }
+
+    const handleFavouriteButton = () => {
+        if (authentication) setIsFavourite(!isFavourite)
+        else {
+            alert()
+        }
+    }
+
+    const handleShareButton = () => {
+        if (authentication) {
+
+        }
+        else {
+            alert()
+        }
+    }
+
+    useEffect(() => {
+        if (isFavourite) {
+            if (!favouriteCourses.some(returnItem => returnItem.id === item.id)) {
+                const newFavourite = favouriteCourses.slice()
+                setFavouriteCourses(newFavourite.concat(item))
+            }
+        } else {
+            const newFavourite = favouriteCourses.filter(returnItem => returnItem.id !== item.id)
+            setFavouriteCourses(newFavourite)
+        }
+    },[isFavourite])
+
+    useEffect(() => {
+        if (isRegistered) {
+            if (!registeredCourses.some(returnItem => returnItem.id === item.id)) {
+                const newRegistered = registeredCourses.slice()
+                setRegisteredCourses(newRegistered.concat(item))
+            }
+        } else {
+            const newRegistered = registeredCourses.filter(returnItem => returnItem.id !== item.id)
+            setRegisteredCourses(newRegistered)
+        }
+    },[isRegistered])
+
+    useEffect(() => {
+        if (isDownloaded) {
+            if (!downloadedCourses.some(returnItem => returnItem.id === item.id)) {
+                const newDownloaded = downloadedCourses.slice()
+                setDownloadedCourses(newDownloaded.concat(item))
+            }
+        } else {
+            const newDownloaded = downloadedCourses.filter(returnItem => returnItem.id !== item.id)
+            setDownloadedCourses(newDownloaded)
+        }
+    },[isDownloaded])
 
     return (
         <ScrollView style={styles(theme).container}>
             <View style={{padding: 5}}>
-                <CourseInfo item={props.route.params.item}/>
-            </View>
-            <View style={{flexDirection: 'row', padding: 5}}>
-                <Text style={{color: theme.normalText}}>{language.rating}</Text>
-                <StarRating
-                    disabled
-                    iconSet={'Ionicons'}
-                    emptyStar={'ios-star-outline'}
-                    fullStar={'ios-star'}
-                    halfStar={'ios-star-half'}
-                    maxStars={5}
-                    rating={4.5}
-                    starSize={18}
-                    fullStarColor={theme.star}
-                    containerStyle={{justifyContent:'flex-start'}}
-                />
+                <CourseInfo item={item}/>
             </View>
             <View style={{flexDirection: "row", justifyContent:'space-evenly', padding: 5}}>
-                <TouchableOpacity style={{flex: 1, borderColor:theme.primaryButton, borderWidth: 2}}>
-                    <Icon type='octicon' name='heart' size={35} color={theme.primaryButton}/>
+                <TouchableOpacity
+                    onPress={handleFavouriteButton}
+                    style={(isFavourite) ? styles(theme).pressedButton : styles(theme).unPressedButton}>
+                    <Icon type='octicon'
+                          name='heart'
+                          size={35}
+                          iconStyle={(isFavourite) ? styles(theme).pressedIcon : styles(theme).unPressedIcon}/>
                 </TouchableOpacity>
-                <TouchableOpacity style={{flex: 1, borderColor:theme.primaryButton, borderWidth: 2}}>
-                    <Icon type='octicon' name='pencil' size={35} color={theme.primaryButton}/>
+                <TouchableOpacity
+                    onPress={handleRegisteredButton}
+                    style={(isRegistered) ? styles(theme).pressedButton : styles(theme).unPressedButton}>
+                    <Icon type='octicon'
+                          name='pencil'
+                          size={35}
+                          iconStyle={(isRegistered) ? styles(theme).pressedIcon : styles(theme).unPressedIcon}/>
                 </TouchableOpacity>
-                <TouchableOpacity style={{flex: 1, borderColor:theme.primaryButton, borderWidth: 2}}>
-                    <Icon type='ionicons' name='cloud-download' size={35} color={theme.primaryButton}/>
+                <TouchableOpacity
+                    onPress={handleDownloadedButton}
+                    style={(isDownloaded) ? styles(theme).pressedButton : styles(theme).unPressedButton}>
+                    <Icon type='ionicons'
+                          name='cloud-download'
+                          size={35}
+                          iconStyle={(isDownloaded) ? styles(theme).pressedIcon : styles(theme).unPressedIcon}/>
                 </TouchableOpacity>
-                <TouchableOpacity style={{flex: 1, borderColor:theme.primaryButton, borderWidth: 2}}>
-                    <Icon type='ionicons' name='share' size={35} color={theme.primaryButton}/>
+                <TouchableOpacity
+                    onPress={handleShareButton}
+                    style={{flex: 1, borderColor:theme.primaryButton, borderWidth: 2}}>
+                    <Icon type='ionicons'
+                          name='share'
+                          size={35}
+                          iconStyle={styles(theme).unPressedIcon}/>
                 </TouchableOpacity>
             </View>
-            <View style={{borderWidth: 2, borderColor: '#000000', height: 500, alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{color: theme.normalText}}>
-                    Course Information
-                </Text>
+            <View style={{padding: 5, paddingTop: 20}}>
+                <ViewMoreText
+                    numberOfLines={5}
+                    renderViewMore={(onPress) => (
+                        <Text onPress={onPress}
+                              style={{color:theme.primaryButton, fontSize: 16}}>
+                            View more
+                        </Text>
+                    )}
+                    renderViewLess={(onPress) => (
+                        <Text onPress={onPress}
+                              style={{color:theme.primaryButton, fontSize: 16}}>
+                            View less
+                        </Text>
+                    )}
+                    textStyle={{color: theme.normalText}}
+                >
+                    <Text>
+                        {item.overview}
+                    </Text>
+                </ViewMoreText>
             </View>
             <View style={{padding: 5}}>
-                <View style={{paddingTop: 20, paddingBottom: 10}}>
+                <View style={{paddingTop: 5, paddingBottom: 10}}>
                     <SectionHeader2 title='Related Courses'/>
                 </View>
                 <HorizontalCourseList
@@ -68,6 +186,23 @@ const styles = (theme) => StyleSheet.create({
     container: {
         backgroundColor: theme.background,
     },
+    pressedButton: {
+        flex: 1,
+        backgroundColor: theme.primaryButton,
+        borderColor:theme.primaryButton,
+        borderWidth: 2
+    },
+    pressedIcon: {
+        color: theme.background
+    },
+    unPressedButton : {
+        flex: 1,
+        borderColor:theme.primaryButton,
+        borderWidth: 2,
+    },
+    unPressedIcon: {
+        color: theme.primaryButton
+    }
 });
 
 export default CourseDetailInformation;
