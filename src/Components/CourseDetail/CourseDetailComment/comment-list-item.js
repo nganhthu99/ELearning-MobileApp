ec9708cd1154c9fbfc9f0e52909bc1a4d0dfa291
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-27 10:53:08 +0700

    Update course detail comment

--- src/Components/CourseDetail/CourseDetailComment/comment-list-item.js
+++ src/Components/CourseDetail/CourseDetailComment/comment-list-item.js
@@ -1,23 +1,43 @@
 import React, {useContext} from 'react';
 import {Image, StyleSheet, Text, View} from "react-native";
 import {ThemeContext} from "../../../Provider/theme-provider";
+import ViewMoreText from 'react-native-view-more-text';
 
 const CommentListItem = (props) => {
     const {theme} = useContext(ThemeContext)
 
     return (
         <View style={styles(theme).container}>
-            <View style={{flex: 2, flexDirection: 'row', alignItems: 'center'}}>
-                <View style={{width: 45}}>
-                    <Image style={{flex: 1, resizeMode: 'contain', borderRadius: 25, borderWidth: 1, borderColor: theme.primaryEmphasis}} source={{uri:'https://i.pinimg.com/originals/8a/f7/ed/8af7ed2daec49e34d1dbfeb52ba7a582.jpg'}}/>
+            <View style={{flex: 2, flexDirection: 'row'}}>
+                <View style={{width: 40, height: 40}}>
+                    <Image style={{flex: 1, resizeMode: 'cover', borderWidth: 1, borderColor: theme.primaryEmphasis}} source={{uri:'https://i.pinimg.com/originals/8a/f7/ed/8af7ed2daec49e34d1dbfeb52ba7a582.jpg'}}/>
                 </View>
                 <View style={{paddingLeft: 10}}>
-                    <Text style={{fontSize: 11, color: theme.normalText}}>{props.item.user}</Text>
+                    <Text style={{fontSize: 11, color: theme.normalText}}>{props.item.username}</Text>
                     <Text style={{fontSize: 11, color: theme.normalText}}>{props.item.time}</Text>
                 </View>
             </View>
-            <View style={{flex: 1, paddingLeft: 55}}>
-                <Text style={{color: theme.normalText}}>{props.item.content}</Text>
+            <View style={{flex: 2, paddingLeft: 50}}>
+                <ViewMoreText
+                    numberOfLines={3}
+                    renderViewMore={(onPress) => (
+                        <Text onPress={onPress}
+                              style={{color:theme.primaryButton, fontSize: 12}}>
+                            View more
+                        </Text>
+                    )}
+                    renderViewLess={(onPress) => (
+                        <Text onPress={onPress}
+                              style={{color:theme.primaryButton, fontSize: 12}}>
+                            View less
+                        </Text>
+                    )}
+                    textStyle={{color: theme.normalText, fontSize: 14}}
+                >
+                    <Text>
+                        {props.item.content}
+                    </Text>
+                </ViewMoreText>
             </View>
         </View>
     )
@@ -27,9 +47,9 @@ const styles = (theme) => StyleSheet.create({
     container: {
         flex:1,
         backgroundColor: theme.background,
-        paddingTop: 10,
-        paddingBottom: 10,
-        borderBottomWidth: 0.5, borderBottomColor: theme.primaryEmphasis
+        borderBottomWidth: 1, borderBottomColor: theme.primaryEmphasis,
+        paddingBottom: 20,
+        paddingTop: 5
     },
 });
 
--- src/Components/CourseDetail/CourseDetailComment/course-detail-comment.js
+++ src/Components/CourseDetail/CourseDetailComment/course-detail-comment.js
@@ -1,36 +1,126 @@
-import React, {useContext} from 'react';
-import {View, StyleSheet, ScrollView, FlatList, Image} from "react-native";
+import React, {useContext, useState} from 'react';
+import {View, StyleSheet, ScrollView, FlatList, Image, Text, TouchableOpacity, Alert} from "react-native";
 import {comments} from "../../../Data/data";
 import CommentListItem from "./comment-list-item";
-import {Input} from "react-native-elements";
+import {Icon, Input} from "react-native-elements";
 import {ThemeContext} from "../../../Provider/theme-provider";
 import {LanguageContext} from "../../../Provider/language-provider";
+import StarRating from "react-native-star-rating";
+import {AuthenticationContext} from "../../../Provider/authentication-provider";
 
 const CourseDetailComment = (props) => {
     const {theme} = useContext(ThemeContext)
     const {language} = useContext(LanguageContext)
+    const {authentication} = useContext(AuthenticationContext)
+    const [starRating, setStarRating] = useState(1)
+    const [comment, setComment] = useState(comments)
+    const [inputComment, setInputComment] = useState("")
+
+    const handleOnStarRatingPress = (rating) => {
+        if (authentication) {
+            setStarRating(rating)
+        } else {
+            Alert.alert(
+                'Authentication Error',
+                'Please Sign In or Sign Up to use this feature',
+                [
+                    {
+                        text: 'Cancel',
+                        onPress: () => console.log('Cancel Pressed'),
+                        style: 'cancel'
+                    }
+                ]
+            );
+        }
+    }
+
+    const handleCommentOnChangeText = (text) => {
+        setInputComment(text)
+    }
+
+    const handleCommentOnSubmit = () => {
+        console.log('heelo')
+        if (inputComment.length > 0) {
+            if (authentication) {
+                const today = new Date();
+                const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                const dateTime = date + ' ' + time;
+
+                const newComment = comment.slice()
+                setComment(newComment.concat({
+                    id: 11,
+                    time: dateTime,
+                    username: authentication.username,
+                    content: inputComment
+                }))
+            } else {
+                Alert.alert(
+                    'Authentication Error',
+                    'Please Sign In or Sign Up to use this feature',
+                    [
+                        {
+                            text: 'Cancel',
+                            onPress: () => console.log('Cancel Pressed'),
+                            style: 'cancel'
+                        }
+                    ]
+                );
+            }
+        }
+    }
 
     const renderItem = ({ item }) => {
         return (
-            <View style={{height: 100, padding: 5}}>
+            <View style={{padding: 5}}>
                 <CommentListItem item={item}/>
             </View>
         )
     };
     return (
         <ScrollView style={styles(theme).container}>
+            <View style={{flexDirection: 'row', padding: 5, paddingTop: 20, paddingBottom: 10}}>
+                <Text style={{color: theme.primaryEmphasis, fontWeight: 'bold', fontSize: 22, paddingRight: 40}}>
+                    {language.ratingCourse}
+                </Text>
+                <StarRating
+                    iconSet={'Ionicons'}
+                    emptyStar={'ios-star-outline'}
+                    fullStar={'ios-star'}
+                    halfStar={'ios-star-half'}
+                    maxStars={5}
+                    rating={starRating}
+                    starSize={28}
+                    fullStarColor={theme.star}
+                    containerStyle={{justifyContent:'flex-start', alignItems: 'center'}}
+                    selectedStar={(rating) => handleOnStarRatingPress(rating)}
+                />
+            </View>
             <View style={styles(theme).commentContainer}>
-                <View style={styles(theme).imageContainer}>
-                    <Image style={styles(theme).image} source={{uri:'https://i.pinimg.com/originals/8a/f7/ed/8af7ed2daec49e34d1dbfeb52ba7a582.jpg'}}/>
-                </View>
-                <View style={styles(theme).inputContainer}>
-                    <Input
-                        placeholder={language.commentInput}
-                        inputStyle={{color: theme.normalText}}/>
-                </View>
+                <Input
+                    multiline
+                    placeholder="Enter you comment"
+                    leftIcon={{ type: 'font-awesome', name: 'comment-o', color: theme.primaryEmphasis}}
+                    style={styles}
+                    label='Comment'
+                    labelStyle={{color: theme.primaryEmphasis, paddingBottom: 10}}
+                    inputStyle={{fontSize: 14, alignSelf: 'flex-end', color: theme.normalText, maxHeight: 200}}
+                    containerStyle={{justifyContent:'center'}}
+                    onChangeText={(text) => handleCommentOnChangeText(text)}
+                    rightIcon={
+                        <TouchableOpacity onPress={handleCommentOnSubmit}>
+                            <Icon
+                                type='font-awesome'
+                                color={theme.primaryEmphasis}
+                                name='paper-plane'
+                                size={20}
+                            />
+                        </TouchableOpacity>
+                    }
+                />
             </View>
             <FlatList
-                data={comments}
+                data={comment}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
             />
@@ -44,27 +134,11 @@ const styles = (theme) => StyleSheet.create({
         backgroundColor: theme.background,
     },
     commentContainer: {
-        height: 120,
-        flexDirection: 'row',
-        alignItems: 'center',
+        borderWidth: 1, borderColor: '#000000', borderRadius: 10,
+        margin: 5,
         padding: 5,
-        paddingTop: 10,
-        paddingBottom: 10,
+        paddingTop: 20
     },
-    imageContainer: {
-        width: 100,
-    },
-    image: {
-        flex: 1,
-        resizeMode: 'contain',
-        borderRadius: 50,
-        borderWidth: 1,
-        borderColor: theme.primaryEmphasis
-    },
-    inputContainer: {
-        flex: 2,
-        alignSelf: 'flex-end'
-    }
 });
 
 export default CourseDetailComment;
