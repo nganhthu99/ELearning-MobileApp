587e3ba325d1509cc459e2acc237bf2db2ba42ae
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-11-28 09:23:18 +0700

    Update app.js, package

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from "@react-navigation/native";
 import StartStack from "./src/Components/Navigation/start-stack";
+import {ThemeProvider} from "./src/Provider/theme-provider";
+import {LanguageProvider} from "./src/Provider/language-provider";
+import {AuthenticationProvider} from "./src/Provider/authentication-provider";
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <StartStack/>
-        </NavigationContainer>
+        <ThemeProvider>
+            <LanguageProvider>
+                <AuthenticationProvider>
+                    <NavigationContainer>
+                        <StartStack/>
+                    </NavigationContainer>
+                </AuthenticationProvider>
+            </LanguageProvider>
+        </ThemeProvider>
     );
 }
