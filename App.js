4c309c3aa1c8daf5d1a76abca06f115710317dbf
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-20 15:50:47 +0700

    Add localization to app

--- App.js
+++ App.js
@@ -2,20 +2,36 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from "@react-navigation/native";
 import StartStack from "./src/Components/Navigation/start-stack";
-import {ThemeProvider} from "./src/Provider/theme-provider";
-import {LanguageProvider} from "./src/Provider/language-provider";
-import {AuthenticationProvider} from "./src/Provider/authentication-provider";
+import {ThemeProvider} from "./src/Core/Provider/theme-provider";
+import {AuthenticationProvider} from "./src/Core/Provider/authentication-provider";
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import english from './src/Globals/Localization/english.json'
+import vietnamese from './src/Globals/Localization/vietnamese.json'
+import {getAllStorageKeys} from "./src/Core/Service/async-storage-service";
 
 export default function App() {
+    i18n.translations = {
+        "en": english,
+        "vi": vietnamese,
+    };
+    // Set the locale once at the beginning of your app.
+    i18n.locale = Localization.locale;
+    // When a value is missing from a language it'll fallback to another language with the key present.
+    i18n.fallbacks = true;
+
+    getAllStorageKeys()
+        .then((value) => {
+            console.log(value)
+        })
+
     return (
         <ThemeProvider>
-            <LanguageProvider>
-                <AuthenticationProvider>
-                    <NavigationContainer>
-                        <StartStack/>
-                    </NavigationContainer>
-                </AuthenticationProvider>
-            </LanguageProvider>
+            <AuthenticationProvider>
+                <NavigationContainer>
+                    <StartStack/>
+                </NavigationContainer>
+            </AuthenticationProvider>
         </ThemeProvider>
     );
 }
