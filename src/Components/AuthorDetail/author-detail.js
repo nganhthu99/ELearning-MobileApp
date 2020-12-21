1f65007909ed13fcd62d92311c08efb2e5595724
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-21 23:33:22 +0700

    Add api call and update author detail screen

--- src/Components/AuthorDetail/author-detail.js
+++ src/Components/AuthorDetail/author-detail.js
@@ -1,36 +1,76 @@
-import React, {useContext} from 'react';
-import {Image, ScrollView, StyleSheet, Text, View} from "react-native";
+import React, {useContext, useState, useEffect} from 'react';
+import {ActivityIndicator, Image, ScrollView, StyleSheet, Text, View} from "react-native";
 import VerticalCourseList from "../CoursesList/VerticalCourseList/vertical-course-list";
-import {courses} from "../../Data/data";
 import SectionHeader2 from "../Common/section-header-2";
-import {ThemeContext} from "../../Provider/theme-provider";
-import {LanguageContext} from "../../Provider/language-provider";
+import {ThemeContext} from "../../Core/Provider/theme-provider";
+import {getIntructorInfo} from "../../Core/Service/instructor-service";
+import {Icon} from "react-native-elements";
+import AuthorDetailNoCourses from "../Common/NoDataView/author-detail-no-courses";
 
 const AuthorDetail = (props) => {
     const {theme} = useContext(ThemeContext)
-    const {language} = useContext(LanguageContext)
+    const [isLoading, setIsLoading] = useState(true)
+    const [detail, setDetail] = useState({})
+    useEffect(() => {
+        getIntructorInfo(props.route.params.itemId)
+            .then((response) => {
+                if (response.status === 200) {
+                    setDetail(response.data.payload)
+                }
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }, [])
 
-    return(
+    if (isLoading) {
+        return (
+            <View style={{marginTop: 20}}>
+                <ActivityIndicator size='small'
+                                   color={theme.emphasis}/>
+            </View>
+        )
+    } else return(
         <ScrollView style={styles(theme).container}>
             {/*image container*/}
             <View style={styles(theme).avatarContainer}>
-                <Image style={styles(theme).image} source={{uri:'https://i.pinimg.com/originals/8a/f7/ed/8af7ed2daec49e34d1dbfeb52ba7a582.jpg'}}/>
+                <Image style={styles(theme).image} source={{uri: detail.avatar}}/>
             </View>
 
-            {/*info container*/}
-            <View style={styles(theme).infoContainer}>
-                <Text style={[styles(theme).text, {fontWeight:'bold', fontSize: 16}]}>{props.route.params.item.name}</Text>
-                <Text style={styles(theme).text}>{`${props.route.params.item.courses} courses`}</Text>
+            {/*general info container*/}
+            <View style={styles(theme).generalInfoContainer}>
+                <Text style={{color: theme.emphasis, fontWeight:'bold', fontSize: 22}}>{detail.name}</Text>
+                <Text style={{color: theme.emphasis, fontSize: 18}}>{detail.major}</Text>
+            </View>
+
+            {/*detail info container*/}
+            <View style={styles(theme).detailInfoContainer}>
+                <Text style={[styles(theme).text, {fontWeight:'bold', fontSize: 14, paddingBottom: 15}]}>{detail.intro}</Text>
+                <Text style={[styles(theme).text, {fontWeight:'bold', fontSize: 14}]}>Skills</Text>
+                <View>
+                    {detail["skills"] && detail["skills"].map(skill => {
+                        return (
+                            <View style={{flexDirection: 'row', marginLeft: 20, marginTop: 5}}>
+                                <Icon type='octicon'
+                                      name='check'
+                                      color={theme.emphasis}/>
+                                <Text style={[styles(theme).text, {marginLeft: 20}]}>
+                                    {skill}
+                                </Text>
+                            </View>
+                        )
+                    })}
+                </View>
             </View>
 
             {/*courses container*/}
             <View style={styles(theme).coursesContainer}>
-                <View style={styles(theme).header}>
-                    <SectionHeader2 title={language.courses}/>
-                </View>
-                <VerticalCourseList
-                    navigation={props.navigation}
-                    items={courses}/>
+                <SectionHeader2 title={`${detail.name}'s courses`}/>
+                {detail.courses &&
+                <VerticalCourseList navigation={props.navigation}
+                                    items={detail.courses}/>}
+                {detail.courses && detail.courses.length === 0 &&
+                <AuthorDetailNoCourses/>}
             </View>
         </ScrollView>
     )
@@ -47,27 +87,31 @@ const styles = (theme) => StyleSheet.create({
     },
     image: {
         flex: 1,
-        resizeMode: 'contain',
+        resizeMode: 'cover',
         width: '100%',
         height: '100%',
-        //borderRadius: 175,
+        borderRadius: 20,
         borderWidth: 2,
-        borderColor: theme.primaryEmphasis,
+        borderColor: theme.emphasis,
         aspectRatio: 1
     },
-    infoContainer: {
+    generalInfoContainer: {
         alignItems: 'center',
-        padding: 10
+        padding: 10,
+    },
+    detailInfoContainer: {
+        padding: 10,
     },
     text: {
-        color: theme.normalText
+        color: theme.text,
+        fontSize: 15,
     },
     header: {
         paddingTop: 15,
         paddingBottom: 10
     },
     coursesContainer: {
-        padding: 5
+        padding: 5,
     }
 });
 
