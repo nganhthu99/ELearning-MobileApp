8d559867424c8e42ea75b0b2f67253cde637b3ea
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-22 23:54:45 +0700

    Update my courses screen

--- src/Components/MyCourses/my-courses.js
+++ src/Components/MyCourses/my-courses.js
@@ -1,100 +1,103 @@
 import React, {useContext} from 'react';
-import {ScrollView, StyleSheet, Text, View} from "react-native";
-import {courses} from "../../Data/data";
+import {ScrollView, StyleSheet, View} from "react-native";
 import HorizontalCourseList from "../CoursesList/HorizontalCourseList/horizontal-course-list";
 import SectionHeader from "../Common/section-header";
 import {ScreenName} from "../../Globals/constants";
-import {ThemeContext} from "../../Provider/theme-provider";
-import {LanguageContext} from "../../Provider/language-provider";
-import {AuthenticationContext} from "../../Provider/authentication-provider";
+import {ThemeContext} from "../../Core/Provider/theme-provider";
+import {AuthenticationContext} from "../../Core/Provider/authentication-provider";
 import UnauthenticationView from "../Common/unauthentication-view";
-import {RegisteredCoursesContext} from "../../Provider/registered-courses-provider";
-import {DownloadedCoursesContext} from "../../Provider/downloaded-courses-provider";
-import {FavouriteCoursesContext} from "../../Provider/favourite-courses-provider";
+import ImageButton from "../Common/image-button";
+import i18n from 'i18n-js';
+import {strings} from "../../Globals/Localization/string";
+import {ContinueCoursesContext} from "../../Core/Provider/continue-courses-provider";
+import {DownloadedCoursesContext} from "../../Core/Provider/downloaded-courses-provider";
+import {FavouriteCoursesContext} from "../../Core/Provider/favourite-courses-provider";
 
 const MyCourses = (props) => {
     const {theme} = useContext(ThemeContext)
-    const {language} = useContext(LanguageContext)
-    const {authentication} = useContext(AuthenticationContext)
-    const {registeredCourses} = useContext(RegisteredCoursesContext)
-    const {downloadedCourses} = useContext(DownloadedCoursesContext)
-    const {favouriteCourses} = useContext(FavouriteCoursesContext)
+    const authenticationContext = useContext(AuthenticationContext)
+    const {continueCourses, setContinueCourses} = useContext(ContinueCoursesContext)
+    const {downloadedCourses, setDownloadedCourses} = useContext(DownloadedCoursesContext)
+    const {favouriteCourses, setFavouriteCourses} = useContext(FavouriteCoursesContext)
+
+    const handleRecommendButton = () => {
+        props.navigation.navigate(ScreenName.CourseList, {
+            header: i18n.t(strings.recommend_for_you),
+            items: []
+        })
+    }
 
     const handleContinueLearningButton = () => {
         props.navigation.navigate(ScreenName.CourseList, {
-            items: courses,
-            header: language.continueLearning,
+            items: continueCourses,
+            header: i18n.t(strings.continue_learning),
         })
     }
 
-    const handleRegisteredButton = () => {
-        props.navigation.navigate(ScreenName.CourseList, {
-            items: registeredCourses,
-            header: language.registered,
-        })
-    }
+    // const handleRegisteredButton = () => {
+    //     props.navigation.navigate(ScreenName.CourseList, {
+    //         items: registeredCourses,
+    //         header: language.registered,
+    //     })
+    // }
 
-    const handleDownloadedButton = () => {
-        props.navigation.navigate(ScreenName.CourseList, {
-            items: downloadedCourses,
-            header: language.downloaded,
-        })
-    }
+    // const handleDownloadedButton = () => {
+    //     props.navigation.navigate(ScreenName.CourseList, {
+    //         items: downloadedCourses,
+    //         header: language.downloaded,
+    //     })
+    // }
 
     const handleFavouriteButton = () => {
         props.navigation.navigate(ScreenName.CourseList, {
             items: favouriteCourses,
-            header: language.favourite,
+            header: i18n.t(strings.favourite),
         })
     }
-    if (authentication) {
+
+    if (!authenticationContext.state.isAuthenticated) {
         return (
-            <ScrollView style={styles(theme).container}>
-                <View style={styles(theme).sectionContainer}>
-                    <View style={styles(theme).header}>
-                        <SectionHeader title={language.continueLearning} handleOnClick={handleContinueLearningButton}/>
-                    </View>
-                    <View>
-                        <HorizontalCourseList
-                            navigation={props.navigation}
-                            items={courses}/>
-                    </View>
-                </View>
-                <View style={styles(theme).sectionContainer}>
-                    <View style={styles(theme).header}>
-                        <SectionHeader title={language.registered} handleOnClick={handleRegisteredButton}/>
-                    </View>
-                    <View>
-                        <HorizontalCourseList
-                            navigation={props.navigation}
-                            items={registeredCourses}/>
-                    </View>
-                </View>
-                <View style={styles(theme).sectionContainer}>
-                    <View style={styles(theme).header}>
-                        <SectionHeader title={language.downloaded} handleOnClick={handleDownloadedButton}/>
-                    </View>
-                    <View>
-                        <HorizontalCourseList
-                            navigation={props.navigation}
-                            items={downloadedCourses}/>
-                    </View>
-                </View>
-                <View style={styles(theme).sectionContainer}>
-                    <View style={styles(theme).header}>
-                        <SectionHeader title={language.favourite} handleOnClick={handleFavouriteButton}/>
-                    </View>
-                    <View>
-                        <HorizontalCourseList
-                            navigation={props.navigation}
-                            items={favouriteCourses}/>
-                    </View>
-                </View>
-            </ScrollView>
+            <UnauthenticationView navigation={props.navigation}/>
         )
     } else {
         return (
-            <UnauthenticationView navigation={props.navigation}/>
+            <ScrollView style={styles(theme).container}>
+                <View style={styles(theme).buttonContainer}>
+                    <ImageButton handleOnClick={handleRecommendButton}
+                                 title={i18n.t(strings.recommend_for_you)}
+                                 image={require('../../../assets/background_3.jpg')}/>
+                </View>
+                <View style={styles(theme).sectionContainer}>
+                    <SectionHeader title={i18n.t(strings.continue_learning)}
+                                   handleOnClick={handleContinueLearningButton}/>
+                    <HorizontalCourseList navigation={props.navigation}
+                                          items={continueCourses}/>
+                </View>
+                {/*<View style={styles(theme).sectionContainer}>*/}
+                {/*    <View style={styles(theme).header}>*/}
+                {/*        <SectionHeader title={language.registered}*/}
+                {/*                       handleOnClick={handleRegisteredButton}/>*/}
+                {/*    </View>*/}
+                {/*    <View>*/}
+                {/*        <HorizontalCourseList*/}
+                {/*            navigation={props.navigation}*/}
+                {/*            items={registeredCourses}/>*/}
+                {/*    </View>*/}
+                {/*</View>*/}
+                {/*<View style={styles(theme).sectionContainer}>*/}
+                {/*    <SectionHeader title={language.downloaded}*/}
+                {/*                   handleOnClick={handleDownloadedButton}/>*/}
+                {/*    <HorizontalCourseList*/}
+                {/*        navigation={props.navigation}*/}
+                {/*        items={downloadedCourses}/>*/}
+                {/*</View>*/}
+                <View style={styles(theme).sectionContainer}>
+                    <SectionHeader title={i18n.t(strings.favourite)}
+                                   handleOnClick={handleFavouriteButton}/>
+                    <HorizontalCourseList navigation={props.navigation}
+                                          items={favouriteCourses}/>
+                </View>
+            </ScrollView>
         )
     }
 };
@@ -103,12 +106,16 @@ const styles = (theme) => StyleSheet.create({
         backgroundColor: theme.background,
     },
     sectionContainer: {
-        padding: 5,
-        paddingBottom: 15,
+        paddingTop: 5,
+        paddingBottom: 10,
     },
     header: {
         paddingTop: 5,
         paddingBottom: 15
-    }
+    },
+    buttonContainer: {
+        height: 180,
+        padding: 5
+    },
 })
 export default MyCourses;
