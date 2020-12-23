42c9ebe992217a97fbd556abf1deb043b17e7b84
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-23 21:33:50 +0700

    Add api call to get course detail and add video components from expo-av and react-native-youtube-iframe

--- src/Components/CourseDetail/course-detail.js
+++ src/Components/CourseDetail/course-detail.js
@@ -1,44 +1,82 @@
-import React, {useState} from 'react';
-import {Linking, StyleSheet, View} from "react-native";
-import CourseDetailTab from "../Navigation/CourseDetail/course-detail-tab";
-import {Button} from "react-native-elements";
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {ActivityIndicator, StyleSheet, View} from "react-native";
+import {Video} from "expo-av";
+import YoutubePlayer from "react-native-youtube-iframe";
+import CourseDetailTab from "./course-detail-tab";
+import {getCourseDetail} from "../../Core/Service/course-service";
+import {ThemeContext} from "../../Core/Provider/theme-provider";
 
 const CourseDetail = (props) => {
-    // const [lesson, setLesson] = useState(0)
-    //
-    // const handleOnChangeLesson = (section) => {
-    //     setLesson(section)
-    // }
+    const {theme} = useContext(ThemeContext)
+    const [isLoading, setIsLoading] = useState(true)
+    const [detail, setDetail] = useState({})
+    const [video, setVideo] = useState(null)
+    const [youtube, setYoutube] = useState(null)
+    let playerRef = useRef(null)
 
-    const [lesson, setLesson] = useState([0])
+    useEffect(() => {
+        getCourseDetail(props.route.params.itemId)
+            .then((response) => {
+                if(response.status === 200) {
+                    setDetail(response.data.payload)
+                    setVideo(response.data.payload.promoVidUrl)
+                }
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }, [])
 
-    const handleOnChangeLesson = (activeSections) => {
-        setLesson(activeSections)
+    const handleOnChangeLesson = (lesson) => {
+        if (lesson.video.videoUrl.includes('youtube')) {
+            setYoutube(lesson.video.videoUrl.substring(25))
+        }
+        setVideo(lesson.video.videoUrl)
     }
 
-    return(
-        <View style={styles.container}>
-            <View style={{borderWidth: 2, borderColor: '#000000', height: 220, alignItems: 'center', justifyContent: 'center'}}>
-                <Button
-                    onPress={() => {
-                        Linking
-                            .openURL(props.route.params.item.youtube)
-                    }}
-                    type="clear"
-                    title={'Lesson ' + Number(lesson[0] + 1) + ' - Youtube Video'}/>
+    if (isLoading) {
+        return (
+            <View style={{marginTop: 40}}>
+                <ActivityIndicator size='small'
+                                   color={theme.emphasis}/>
             </View>
-            <CourseDetailTab
-                handleOnChangeLesson={handleOnChangeLesson}
-                navigation={props.navigation}
-                item={props.route.params.item}/>
-        </View>
-    )
-};
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                {!youtube &&
+                <Video
+                    source={{uri: video}}
+                    shouldPlay={false}
+                    useNativeControls
+                    ref={playerRef}
+                    style={{height: 220}}/>}
+                {youtube &&
+                <YoutubePlayer
+                    videoId={youtube}
+                    play={false}
+                    ref={playerRef}
+                    height={220}
+                    volume={50}
+                    playbackRate={1}
+                />}
+                {/*<Button title='Seek to'*/}
+                {/*        onPress={async () => {await playerRef.current.setPositionAsync(15000)}}/>*/}
+                <CourseDetailTab
+                    handleOnChangeLesson={handleOnChangeLesson}
+                    navigation={props.navigation}
+                    itemId={props.route.params.itemId}
+                    detail={detail}
+                />
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#F2F2F2',
+        paddingTop: 20
     },
 });
 
