7bf2dbcc9811776870d50c9a59a9c386964899ed
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-24 08:31:20 +0700

    Add api call and update ui course detail exercise

--- src/Components/CourseDetail/CourseDetailExercise/course-detail-exercise.js
+++ src/Components/CourseDetail/CourseDetailExercise/course-detail-exercise.js
@@ -1,27 +1,86 @@
-import React, {useContext, useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from "react-native";
-import {ThemeContext} from "../../../Provider/theme-provider";
+import React, {useContext, useEffect, useState} from 'react';
+import {ActivityIndicator, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {ThemeContext} from "../../../Core/Provider/theme-provider";
 import Accordion from 'react-native-collapsible/Accordion';
-import {courses} from "../../../Data/data";
+import {AuthenticationContext} from "../../../Core/Provider/authentication-provider";
+import {getExerciseQuestions, getLessonExercises} from "../../../Core/Service/course-service";
+import {CurrentLessonContext} from "../../../Core/Provider/current-lesson-provider";
+import NoDataView from "../../Common/no-data-view";
 
 const CourseDetailExercise= (props) => {
     const {theme} = useContext(ThemeContext)
-    const [activeSections, setActiveSections] = useState([])
+    const authenticationContext = useContext(AuthenticationContext)
+    const [isLoading, setIsLoading] = useState(true)
+    const [activeSections, setActiveSections] = useState([0])
+    const [exerciseSections, setExerciseSections] = useState([])
 
-    const SECTIONS = courses[0].exercise
+    const {currentLesson, setCurrentLesson} = useContext(CurrentLessonContext)
 
-    const _renderHeader = section => {
+    useEffect(() => {
+        if (currentLesson.id) {
+            getLessonExercises(authenticationContext.state.token, currentLesson.id)
+                .then((response) => {
+                    if (response.status === 200) {
+                        setExerciseSections(response.data.payload.exercises)
+                    }
+                })
+        }
+    }, [currentLesson])
+
+    useEffect(() => {
+        exerciseSections.map((exercise) => {
+            getExerciseQuestions(authenticationContext.state.token, exercise.id)
+                .then((response) => {
+                    if (response.status === 200) {
+                        exercise.exercises_questions = response.data.payload.exercises.exercises_questions
+                    }
+                })
+        })
+        setIsLoading(false)
+    }, [exerciseSections])
+
+    const _renderHeader = (section, index) => {
         return (
-            <View style={{borderTopWidth: 1, borderTopColor: theme.primaryButton, borderBottomWidth: 1, borderBottomColor: theme.primaryButton, backgroundColor: theme.primaryButton, justifyContent:'center', paddingTop: 10, paddingBottom: 10}}>
-                <Text style={{color: theme.background, fontSize: 16, fontWeight: 'bold', paddingLeft: 5, paddingRight: 5}}>{`${section.id}.  ${section.title}`}</Text>
+            <View
+                key={section.id}
+                style={{backgroundColor: theme.background, justifyContent:'center', paddingTop: 10, paddingBottom: 5}}>
+                <Text style={{color: theme.primary, fontSize: 15, fontWeight: 'bold', paddingLeft: 5, paddingRight: 5}}>
+                    {`Exercise ${index + 1} - ${section.title}`}
+                </Text>
             </View>
         );
     };
 
     const _renderContent = section => {
+        const renderQuestionItem = (question, index) => {
+            return (
+                <View style={{padding: 5}}>
+                    <View style={{backgroundColor: theme.primary, padding: 5, flexDirection: 'row'}}>
+                        <Text style={{color: theme.background, fontWeight: 'bold', fontSize: 12}}>
+                            {`${index + 1}. ${question.content}`}
+                        </Text>
+                    </View>
+                    <View style={{padding: 10}}>
+                        {question.exercises_answers && question.exercises_answers.map((answer, index) => {
+                            return (
+                                <TouchableOpacity key={answer.id}>
+                                    <Text style={{paddingBottom: 5}}>
+                                        {`${String.fromCharCode(index + 65)}. ${answer.content}`}
+                                    </Text>
+                                </TouchableOpacity>
+                            )
+                        })}
+                    </View>
+                </View>
+            )
+        }
         return (
-            <View style={{paddingLeft: 5, paddingRight: 5, paddingTop: 10}}>
-                <Text style={{color: theme.normalText}}>{section.content}</Text>
+            <View
+                key={section.id}
+                style={{paddingTop: 5}}>
+                {section.exercises_questions.map((question, index) => {
+                    return renderQuestionItem(question, index)
+                })}
             </View>
         );
     };
@@ -30,22 +89,36 @@ const CourseDetailExercise= (props) => {
         setActiveSections(activeSections);
     };
 
-    return (
-        <ScrollView style={styles(theme).container}>
-            <Accordion
-                sections={SECTIONS}
-                activeSections={activeSections}
-                renderHeader={_renderHeader}
-                renderContent={_renderContent}
-                onChange={_updateSections}
-            />
-        </ScrollView>
-    )
+    if (isLoading) {
+        return (
+            <View style={{marginTop: 20}}>
+                <ActivityIndicator size='small'
+                                   color={theme.emphasis}/>
+            </View>
+        )
+    } else {
+        return (
+            <ScrollView style={styles(theme).container}>
+                {exerciseSections.length > 0 &&
+                <Accordion
+                    sections={exerciseSections}
+                    activeSections={activeSections}
+                    renderHeader={_renderHeader}
+                    renderContent={_renderContent}
+                    onChange={_updateSections}
+                    expandMultiple
+                />}
+                {exerciseSections.length === 0 && <NoDataView message='This lesson has no exercises available.'/>}
+            </ScrollView>
+        )
+    }
 };
 
 const styles = (theme) => StyleSheet.create({
     container: {
         backgroundColor: theme.background,
+        paddingLeft: 5,
+        paddingRight: 5
     },
 });
 
