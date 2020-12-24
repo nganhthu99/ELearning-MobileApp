1cc444bb96e7f8761f37a084f4748d86adcb68ce
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-24 08:30:34 +0700

    Update comment list item ui

--- src/Components/CourseDetail/CourseDetailComment/comment-list-item.js
+++ src/Components/CourseDetail/CourseDetailComment/comment-list-item.js
@@ -1,39 +1,52 @@
 import React, {useContext} from 'react';
 import {Image, StyleSheet, Text, View} from "react-native";
-import {ThemeContext} from "../../../Provider/theme-provider";
+import {ThemeContext} from "../../../Core/Provider/theme-provider";
 import ViewMoreText from 'react-native-view-more-text';
+import StarRating from "react-native-star-rating";
 
 const CommentListItem = (props) => {
     const {theme} = useContext(ThemeContext)
 
     return (
         <View style={styles(theme).container}>
-            <View style={{flex: 2, flexDirection: 'row'}}>
+            <View style={{flex: 2, flexDirection: 'row', alignItems: 'center'}}>
                 <View style={{width: 40, height: 40}}>
-                    <Image style={{flex: 1, resizeMode: 'cover', borderWidth: 1, borderColor: theme.primaryEmphasis}} source={{uri:'https://i.pinimg.com/originals/8a/f7/ed/8af7ed2daec49e34d1dbfeb52ba7a582.jpg'}}/>
+                    <Image style={{flex: 1, resizeMode: 'cover', width: '100%', height: '100%', borderRadius: 6, borderWidth: 1, borderColor: theme.emphasis, aspectRatio: 1}}
+                           source={{uri:props.item.user.avatar}}/>
                 </View>
                 <View style={{paddingLeft: 10}}>
-                    <Text style={{fontSize: 11, color: theme.normalText}}>{props.item.username}</Text>
-                    <Text style={{fontSize: 11, color: theme.normalText}}>{props.item.time}</Text>
+                    <Text style={{fontSize: 10, color: theme.text}}>{props.item.user.name}</Text>
+                    <Text style={{fontSize: 10, color: theme.text}}>{props.item["updatedAt"].substring(0, 10)}</Text>
                 </View>
             </View>
-            <View style={{flex: 2, paddingLeft: 50}}>
+            <View style={{flex: 2, paddingLeft: 50, paddingTop: 5}}>
+                <StarRating
+                    disabled
+                    iconSet={'Ionicons'}
+                    emptyStar={'ios-star-outline'}
+                    fullStar={'ios-star'}
+                    halfStar={'ios-star-half'}
+                    maxStars={5}
+                    rating={props.item["averagePoint"]}
+                    starSize={18}
+                    fullStarColor={theme.bright}
+                    buttonStyle={{margin: 0.5}}
+                    containerStyle={{justifyContent:'flex-start'}}/>
                 <ViewMoreText
                     numberOfLines={3}
                     renderViewMore={(onPress) => (
                         <Text onPress={onPress}
-                              style={{color:theme.primaryButton, fontSize: 12}}>
+                              style={{color:theme.primary, fontSize: 10}}>
                             View more
                         </Text>
                     )}
                     renderViewLess={(onPress) => (
                         <Text onPress={onPress}
-                              style={{color:theme.primaryButton, fontSize: 12}}>
+                              style={{color:theme.primary, fontSize: 10}}>
                             View less
                         </Text>
                     )}
-                    textStyle={{color: theme.normalText, fontSize: 14}}
-                >
+                    textStyle={{color: theme.text, fontSize: 13}}>
                     <Text>
                         {props.item.content}
                     </Text>
@@ -47,9 +60,8 @@ const styles = (theme) => StyleSheet.create({
     container: {
         flex:1,
         backgroundColor: theme.background,
-        borderBottomWidth: 1, borderBottomColor: theme.primaryEmphasis,
-        paddingBottom: 20,
-        paddingTop: 5
+        paddingTop: 5,
+        paddingBottom: 5
     },
 });
 
