960f069f3a260eb80bd6f44affa52c5fe11fda2d
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-25 10:10:34 +0700

    Update Async Storage service, add set, get ‘downloaded_video’

--- src/Core/Service/async-storage-service.js
+++ src/Core/Service/async-storage-service.js
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as FileSystem from "expo-file-system";
 
 export const getAllStorageKeys = () => {
     return AsyncStorage.getAllKeys()
@@ -25,13 +26,33 @@ export const setStorageTheme = (theme) => {
 }
 
 export const getStorageSearchHistory = () => {
-    return AsyncStorage.getItem('@search_history')
+    return AsyncStorage.getItem('search_history')
 }
 
 export const setStorageSearchHistory = (search) => {
-    // getStorageSearchHistory()
+    getStorageSearchHistory()
+        .then((value) => {
+            return AsyncStorage.setItem('search_history', [...value, search])
+        })
+    // return AsyncStorage.setItem(prevHis => [...prevHis, search])
+}
+
+export const getStorageDownloadedVideo = () => {
+    return AsyncStorage.getItem('downloaded_video')
+}
+
+export const setStorageDownloadedVideo = async (lesson) => {
+    // return AsyncStorage.setItem(video => [...prevHis, search])
+    // getStorageDownloadedVideo()
     //     .then((value) => {
-    //         return AsyncStorage.setItem('search_history', [...value, search])
+    //         if (!value) {
+    //             return AsyncStorage.setItem('downloaded_video', [])
+    //         } else {
+    //             return AsyncStorage.setItem('downloaded_video', value.concat(video))
+    //         }
     //     })
-    return AsyncStorage.setItem(prevHis => [...prevHis, search])
+    const value = await getStorageDownloadedVideo()
+    return (value) ?
+        AsyncStorage.setItem('downloaded_video', JSON.stringify(JSON.parse(value).concat(lesson))) :
+        AsyncStorage.setItem('downloaded_video', JSON.stringify([]))
 }
