16f4f05c6726d251cb8a1f54742baaae93a073cf
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-26 08:38:10 +0700

    Add download lesson detail screen

---
+++ src/Components/CourseDetail/downloaded-lesson-detail.js
@@ -0,0 +1,40 @@
+import React, {useContext, useRef} from 'react';
+import {StyleSheet, Text, View} from "react-native";
+import {Video} from "expo-av";
+import {ThemeContext} from "../../Core/Provider/theme-provider";
+
+const DownloadedLessonDetail = (props) => {
+    const {theme} = useContext(ThemeContext)
+    let playerRef = useRef(null)
+    console.log(props.route.params.item.video.videoUrl)
+    return (
+        <View style={styles.container}>
+            <Video
+                source={{uri: props.route.params.item.video.videoUrl}}
+                shouldPlay={false}
+                useNativeControls
+                ref={playerRef}
+                style={{
+                    backgroundColor: '#111111',
+                    height: 220
+                }}/>
+            <View style={{padding: 5}}>
+                <Text style={{fontSize: 20, paddingBottom: 5 , color: theme.text}}>
+                    {props.route.params.item.name}
+                </Text>
+                <Text style={{fontSize: 14 , color: theme.text}}>
+                    {`${props.route.params.item.hours} hours`}
+                </Text>
+            </View>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20
+    },
+});
+
+export default DownloadedLessonDetail;
