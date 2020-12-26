35d66b96a946ff62543ce43af26d3030592f9fc7
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-26 08:38:27 +0700

    Update course service

--- src/Core/Service/course-service.js
+++ src/Core/Service/course-service.js
@@ -22,33 +22,29 @@ export const getTopRatingCoursesService = (limit, page) => {
     })
 }
 
-export const getRecommendedCoursesService = (limit, offset, userId) => {
-    offset -= 1
-    offset *= limit
+export const getRecommendedCoursesService = (limit, page, userId) => {
+    const offset = limit * (page - 1)
     return axios.get(api + '/user/recommend-course/' + userId + '/' + limit + '/' + offset)
 }
 
-export const getContinueCoursesService = (limit, page, userId, token) => {
+export const getContinueCoursesService = (token) => {
     return axios.get(api + '/user/get-process-courses', {
         headers: {
             Authorization: 'Bearer ' + token
-        },
-        validateStatus: () => true
+        }
     })
 }
 
-export const getFavoriteCoursesService = (limit, page, userId, token) => {
+export const getFavoriteCoursesService = (token) => {
     return axios.get(api + '/user/get-favorite-courses',{
         headers: {
             Authorization: 'Bearer ' + token
-        },
-        validateStatus: () => true
+        }
     })
 }
 
-export const getCategoryCoursesService = (limit, offset, categoryId) => {
-    offset -= 1
-    offset *= limit
+export const getCategoryCoursesService = (limit, page, categoryId) => {
+    const offset = limit * (page - 1)
     return axios.post(api + '/course/search', {
         keyword: "",
         opt: {
@@ -58,19 +54,84 @@ export const getCategoryCoursesService = (limit, offset, categoryId) => {
         },
         limit,
         offset,
-    }, {
-        validateStatus: () => true
+    })
+}
+
+export const getUserCourseFavouriteStatus = (courseId, token) => {
+    return axios.get(api + '/user/get-course-like-status/'+courseId,{
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
     })
 }
 
 export const updateCourseFavouriteStatus = (courseId, token) => {
-    console.log('hereherehere')
     return axios.post(api + '/user/like-course', {
         courseId,
     },{
         headers: {
             Authorization: 'Bearer ' + token
-        },
-        validateStatus: () => true
+        }
+    })
+}
+
+export const searchService = (keyword) => {
+    return axios.post(api+'/course/searchV2', {
+        keyword
+    })
+};
+
+export const getCourseDetail = (courseId) => {
+    return axios.get(api + '/course/get-course-detail/'+courseId+'/null')
+}
+
+export const getUserRatingCourse = (token, courseId) => {
+    return axios.get(api + '/course/get-rating/' + courseId, {
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    })
+}
+
+export const updateUserRatingCourse = (token, courseId, formalityPoint, contentPoint, presentationPoint, content) => {
+    console.log('courseId', courseId)
+    return axios.post(api + '/course/rating-course', {
+        courseId,
+        formalityPoint,
+        contentPoint,
+        presentationPoint,
+        content
+    },{
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    })
+}
+
+export const getLessonWithVideoUrl = (token, courseId, lessonId) => {
+    return axios.get(api + '/lesson/video/'+courseId+'/'+lessonId, {
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    })
+}
+
+export const getLessonExercises = (token, lessonId) => {
+    return axios.post(api + '/exercise/student/list-exercise-lesson',{
+        lessonId,
+    },{
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    })
+}
+
+export const getExerciseQuestions = (token, exerciseId) => {
+    return axios.post(api + '/exercise/student/exercise-test',{
+        exerciseId,
+    },{
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
     })
 }
