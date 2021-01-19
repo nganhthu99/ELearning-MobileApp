8417668b2c6914dfa57f1aa2c1f88255ab4fee8a
Thu Anh Nguyen <nganhthu99@gmail.com>
2021-01-19 21:45:57 +0700

    Update App.js and package.json and yarn.lock

--- App.js
+++ App.js
@@ -8,16 +8,11 @@ import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 import english from './src/Globals/Localization/english.json'
 import vietnamese from './src/Globals/Localization/vietnamese.json'
-import {
-    getAllStorageKeys,
-    getStorageDownloadedVideo,
-    setStorageDownloadedVideo
-} from "./src/Core/Service/async-storage-service";
 import * as FileSystem from "expo-file-system";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {DownloadedCoursesProvider} from "./src/Core/Provider/downloaded-courses-provider";
 import {ContinueCoursesProvider} from "./src/Core/Provider/continue-courses-provider";
 import {FavouriteCoursesProvider} from "./src/Core/Provider/favourite-courses-provider";
+import {clearAllStorageKeys, getAllStorageKeys} from "./src/Core/Service/storage-service";
 
 export default function App() {
     i18n.translations = {
@@ -29,37 +24,12 @@ export default function App() {
     // When a value is missing from a language it'll fallback to another language with the key present.
     i18n.fallbacks = true;
 
-    getAllStorageKeys()
-        .then((value) => {
-            console.log(value)
-        })
-
-    // console.log(FileSystem.documentDirectory + 'bluedu-app/downloaded-video/')
-    // FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'bluedu-app/')
-    //     .then(result => {
-    //         console.log('make directory: ', result)
-    //     })
-    //     .catch((error) => {
-    //         console.log('make directory error: ', error)
+    // clearAllStorageKeys()
+    //     .then(() => {
+    //         console.log("CLEAR ALL STORAGE DONE!")
     //     })
 
-    // FileSystem.readAsStringAsync(FileSystem.documentDirectory)
-    //     .then(result => {
-    //         console.log('read directory: ', result)
-    //     })
-    //     .catch((error) => {
-    //         console.log('read directory error: ', error)
-    //     })
-
-    // FileSystem.getInfoAsync(FileSystem.documentDirectory + 'bluedu-app/')
-    //     .then(result => {
-    //         console.log('read directory: ', result)
-    //     })
-    //     .catch((error) => {
-    //         console.log('read directory error: ', error)
-    //     })
-
-    // FileSystem.deleteAsync(FileSystem.documentDirectory + 'fa77a36e-bc91-4fe0-a2a4-4ce38f0b46bb.mov')
+    // FileSystem.deleteAsync(FileSystem.documentDirectory + "a4a4af67-c240-4ced-9884-868ffc0c95bd.mp4")
     //     .then(result => {
     //         console.log('delete directory: ', result)
     //     })
@@ -67,33 +37,17 @@ export default function App() {
     //         console.log('delete directory error: ', error)
     //     })
 
-    // getStorageDownloadedVideo()
-    //     .then((value) => {
-    //         if (!value) {
-    //             AsyncStorage.setItem('downloaded_video', [])
-    //                 .then(() => {console.log('hehehe')})
-    //         } else {
-    //             AsyncStorage.setItem('downloaded_video', value.concat(video))
-    //                 .then(() => {console.log('huhuhu')})
-    //         }
-    //     })
-
-    // AsyncStorage.getItem('downloaded_video')
-    //     .then((value) => {
-    //         if (!value)
-    //             AsyncStorage.setItem('downloaded_video', JSON.stringify([]))
-    //         else
-    //             AsyncStorage.setItem('downloaded_video', JSON.stringify(value.concat(JSON.stringify({id: 1, title: 'hello'}))))
-    //     })
-
-    // AsyncStorage.removeItem('downloaded_video')
-
-    FileSystem.getInfoAsync(FileSystem.documentDirectory)
+    FileSystem.readDirectoryAsync(FileSystem.documentDirectory)
         .then(result => {
-            console.log('directory info: ', result)
+            console.log('DOCUMENT DIRECTORY INFO: ', result)
         })
         .catch((error) => {
-            console.log('directory error: ', error)
+            console.log('DOCUMENT DIRECTORY error: ', error)
+        })
+
+    getAllStorageKeys()
+        .then((value) => {
+            console.log('ASYNC STORAGE KEYS: ', value)
         })
 
     return (
